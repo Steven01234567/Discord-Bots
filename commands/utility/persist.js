329df06560b15ab1ef2759fb2fb16d0acbc20351
Steven Chen <steven.bin.chen17@gmail.com>
2025-03-04 14:13:50 -0500

    added way to end persist loop

--- commands/utility/persist.js
+++ commands/utility/persist.js
@@ -1,3 +1,4 @@
+let persistSwitch = false;
 const { SlashCommandBuilder, userMention } = require('discord.js');
 
 module.exports = {
@@ -7,17 +8,29 @@ module.exports = {
 		.addUserOption(option =>
 			option.setName('target')
 				.setDescription('The user to annoy')
-		        .setRequired(true)),
+		        .setRequired(false))
+		.addBooleanOption(option =>
+			option.setName('end')
+				.setDescription('Ends persist')
+				.setRequired(false)),
 	async execute(interaction) {
 		const wait = require('node:timers/promises').setTimeout;
 		const delay = 1000;
 
 		const target = interaction.options.getUser('target');
 
-		await interaction.reply(userMention(target.id) + ' Hellooooo!');
-		while (true) {
-			await wait(delay);
-		    await interaction.followUp(userMention(target.id) + ' Hellooooo!');
+		if (interaction.options.getBoolean('end')) {
+			persistSwitch = false;
+		}
+		else {
+			persistSwitch = true;
+
+			await interaction.reply(userMention(target.id) + ' Hellooooo!');
+			while (persistSwitch) {
+				await wait(delay);
+		    	await interaction.followUp(userMention(target.id) + ' Hellooooo!');
+			}
+			await interaction.followUp(userMention(target.id) + ' Helloooo...');
 		}
 	},
 };
