a0504fd36144bd6892dda63499aabdc3f9516f4e
Steven Chen <steven.bin.chen17@gmail.com>
2025-03-07 18:20:16 -0500

    working on audio playing, not sure what's wrong yet

--- commands/utility/join.js
+++ commands/utility/join.js
@@ -1,5 +1,7 @@
-const { SlashCommandBuilder, ChannelType } = require('discord.js');
-const { joinVoiceChannel, /*getVoiceConnection,*/ createAudioPlayer } = require('@discordjs/voice');
+const { SlashCommandBuilder, ChannelType, generateDependencyReport } = require('discord.js');
+const { joinVoiceChannel, /*getVoiceConnection,*/ createAudioPlayer, createAudioResource /*, StreamType, NoSubscriberBehavior*/ } = require('@discordjs/voice');
+// const { createReadStream } = require('node:fs');
+const { join } = require('node:path');
 
 module.exports = {
 	data: new SlashCommandBuilder()
@@ -22,5 +24,11 @@ module.exports = {
 		// console.log(getVoiceConnection(voiceChannel.guildId));
 
 		const player = createAudioPlayer();
+		const summonSound = createAudioResource(join(__dirname, 'Annoy-o-Tron_Summon.mp3'));
+		player.play(summonSound);
+		const subscription = voiceConnection.subscribe(player);
+		// console.log(summonSound);
+		console.log(generateDependencyReport());
+		console.log('The audio player has started playing!');
 	},
 };
